f61336f9e38c3b2b6779ef1b1b3b54255ca46e50
NicholasPaparusso <nicopaparusso@gmail.com>
2024-02-18 12:37:03 +0100

    [Create] first next app

---
+++ src/app/blog/[slug]/page.js
@@ -0,0 +1,12 @@
+export default function BlogPostPage({params}){
+    return(
+        <main>
+            <h1>
+                Blog post
+            </h1>
+            <p>
+                {params.slug}
+            </p>
+        </main>
+    );
+}
